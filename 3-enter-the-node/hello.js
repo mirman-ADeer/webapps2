68de67f11242b85b67d0e01c3af8a65d0d846d94
August Deer <audeer2019@audeer2019.local>
2016-10-25 14:38:11 -0700

    Bonus Objectives

--- 3-enter-the-node/hello.js
+++ 3-enter-the-node/hello.js
@@ -1,10 +1,18 @@
-var express = require('express');
-var app = express()
+var express = require("express");
+var port = 3000
+var app = express();
 
-app.get('/', function (req, res) {
-	res.send('Hello World!');
+app.get("/", function(req, res) {
+	res.send("Hello from Node!");
 });
 
-app.listen(3000, function () {
-	console.log('Example app listening on port 3000!');
+app.get("/greet/:name", function(req, res) {
+	var name = req.params.name;
+	res.send("<h1>Hi, " + name + "!</h1>");
+});
+
+app.use(express.static("public"));
+
+app.listen(port, function() {
+	console.log("Listening on port " + port.toString());
 });
