e5c83850f2b9dc32a237917f6ae47c588a88c167
August Deer <audeer2019@audeer2019.local>
2016-10-26 12:47:12 -0700

    rerout home to index.html

--- 3-enter-the-node/hello.js
+++ 3-enter-the-node/hello.js
@@ -2,16 +2,12 @@ var express = require("express");
 var port = 3000
 var app = express();
 
-app.get("/", function(req, res) {
-	res.send("Hello from Node!");
-});
-
 app.get("/greet/:name", function(req, res) {
 	var name = req.params.name;
 	res.send("<h1>Hi, " + name + "!</h1>");
 });
 
-app.use(express.static("public"));
+app.use("/", express.static("public"));
 
 app.listen(port, function() {
 	console.log("Listening on port " + port.toString());
